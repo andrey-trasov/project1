4a7ef9a8b618ad471105d1f7f0781687cbb53957
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2024-02-16 18:47:33 +0300

    capital letters

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def large_letters(letters):
     return letters.upper()
+
+
+def capital_letters(capital):
+    """
+    возвращает строку с первыми заглавными буквами
+    """
+    return capital.title()
